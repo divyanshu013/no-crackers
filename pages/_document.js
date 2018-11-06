4d45efc1ea207db290aea77f95cc7640dad4e8ca
Divyanshu Maithani <div.blackcat@gmail.com>
2018-11-07 01:22:42 +0530

    Add twitter og tags

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,17 @@ export default class MyDocument extends Document {
       <html lang="en">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="No Crackers" />
+          <meta
+            name="twitter:description"
+            content="Join the No Crackers pledge. Help spread the word!"
+          />
+          <meta name="twitter:creator" content="@divyanshu013 @ekrysis" />
+          <meta
+            name="twitter:image"
+            content="https://no-crackers.netlify.com/static/og.png"
+          />
           <meta property="og:title" content="No Crackers" />
           <meta
             property="og:description"
